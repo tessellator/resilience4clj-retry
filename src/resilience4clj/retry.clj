da77d4fada667a49feb2d655df6b4c6a66210b36
Chad Taylor <chadtaylor@hey.com>
2022-03-31 20:12:36 -0500

    Update API and add tests

--- src/resilience4clj/retry.clj
+++ src/resilience4clj/retry.clj
@@ -1,49 +1,58 @@
 (ns resilience4clj.retry
   "Functions to create and execute retrys."
-  (:require [clojure.spec.alpha :as s])
-  (:import [io.github.resilience4j.retry
+  (:refer-clojure :exclude [find name reset!])
+  (:require [clojure.core.async :as async]
+            [clojure.string :as str])
+  (:import [io.github.resilience4j.core
+            EventConsumer
+            IntervalBiFunction
             IntervalFunction
+            Registry$EventPublisher]
+           [io.github.resilience4j.core.registry
+            EntryAddedEvent
+            EntryRemovedEvent
+            EntryReplacedEvent]
+           [io.github.resilience4j.retry
             Retry
+            Retry$EventPublisher
             RetryConfig
             RetryRegistry]
+           [io.github.resilience4j.retry.event
+            AbstractRetryEvent
+            RetryOnRetryEvent]
            [java.time Duration]
-           [java.util.function
-            Function
-            Predicate]))
+           [java.util Map Optional]
+           [java.util.function Predicate]))
+
+(set! *warn-on-reflection* true)
+
+(defn- optional-value [^Optional optional]
+  (when (.isPresent optional)
+    (.get optional)))
+
+(defn- name? [val]
+  (or (string? val)
+      (keyword? val)))
+
+(defn- keywordize-enum-value [^Object enum-value]
+  (-> (.toString enum-value)
+      (str/lower-case)
+      (str/replace #"_" "-")
+      (keyword)))
 
 ;; -----------------------------------------------------------------------------
 ;; configuration
-
-(s/def ::max-attempts nat-int?)
-(s/def ::wait-duration nat-int?)
-(s/def ::interval-function (s/or :interval-function #(instance? IntervalFunction %)
-                                 :fn fn?))
-(s/def ::retry-on-result-predicate fn?)
-(s/def ::retry-on-exception-predicate fn?)
-(s/def ::retry-exceptions (s/coll-of class?))
-(s/def ::ignore-exceptions (s/coll-of class?))
-
-(s/def ::config
-  (s/keys :opt-un [::max-attempts
-                   ::wait-duration
-                   ::interval-function
-                   ::retry-on-result-predicate
-                   ::retry-on-exception-predicate
-                   ::retry-exceptions
-                   ::ignore-exceptions]))
-
-(s/def ::name
-  (s/or :string (s/and string? not-empty)
-        :keyword keyword?))
 
 (defn- build-config [config]
   (let [{:keys [max-attempts
                 wait-duration
                 interval-function
+                interval-bi-function
                 retry-on-result-predicate
-                retry-on-exception-predicate
+                retry-exception-predicate
                 retry-exceptions
-                ignore-exceptions]} config]
+                ignore-exceptions
+                fail-after-max-attempts]} config]
     (cond-> (RetryConfig/custom)
 
       max-attempts
@@ -53,26 +62,31 @@
       (.waitDuration (Duration/ofMillis wait-duration))
 
       interval-function
-      (as-> rc
-          (let [[t f] (s/conform ::interval-function interval-function)]
-            (condp = t
-              :interval-function
-              (.intervalFunction rc f)
+      (as-> config
+            (if (instance? IntervalFunction interval-function)
+              (.intervalFunction config interval-function)
+              (.intervalFunction config (reify IntervalFunction (apply [_ num-attempts] (interval-function num-attempts))))))
 
-              :fn
-              (.intervalFunction rc (reify Function (apply [_ num-attempts] (f num-attempts)))))))
+      interval-bi-function
+      (as-> config
+            (if (instance? IntervalBiFunction interval-bi-function)
+              (.intervalBiFunction config interval-bi-function)
+              (.intervalBiFunction config (reify IntervalBiFunction (apply [_ num-attempts throwable-or-result] (interval-bi-function num-attempts throwable-or-result))))))
 
       retry-on-result-predicate
       (.retryOnResult (reify Predicate (test [_ result] (retry-on-result-predicate result))))
 
-      retry-on-exception-predicate
-      (.retryOnException (reify Predicate (test [_ ex] (retry-on-exception-predicate ex))))
+      retry-exception-predicate
+      (.retryOnException (reify Predicate (test [_ ex] (retry-exception-predicate ex))))
 
       retry-exceptions
       (.retryExceptions (into-array java.lang.Class retry-exceptions))
 
       ignore-exceptions
       (.ignoreExceptions (into-array java.lang.Class ignore-exceptions))
+
+      fail-after-max-attempts
+      (.failAfterMaxAttempts fail-after-max-attempts)
 
       :always
       (.build))))
@@ -82,82 +96,183 @@
 
 (defn interval
   "Creates an interval function."
-  [interval-millis]
+  [^long interval-millis]
   (IntervalFunction/of interval-millis))
 
 (defn randomized
   "Creates a randomized interval function."
-  ([interval-millis]
+  ([^long interval-millis]
    (IntervalFunction/ofRandomized interval-millis))
-  ([interval-millis randomization-factor]
+  ([^long interval-millis ^double randomization-factor]
    (IntervalFunction/ofRandomized interval-millis randomization-factor)))
 
 (defn exponential-backoff
   "Creates an exponential backoff interval function."
-  ([initial-interval-millis]
+  ([^long initial-interval-millis]
    (IntervalFunction/ofExponentialBackoff initial-interval-millis))
-  ([initial-interval-millis multiplier]
+  ([^long initial-interval-millis ^double multiplier]
    (IntervalFunction/ofExponentialBackoff initial-interval-millis multiplier)))
 
 (defn exponential-random-backoff
   "Creates an exponential random backoff interval function."
-  ([initial-interval-millis]
+  ([^long initial-interval-millis]
    (IntervalFunction/ofExponentialRandomBackoff initial-interval-millis))
-  ([initial-interval-millis multiplier]
+  ([^long initial-interval-millis ^double multiplier]
    (IntervalFunction/ofExponentialRandomBackoff initial-interval-millis multiplier))
-  ([initial-interval-millis multiplier randomization-factor]
+  ([^long initial-interval-millis ^double multiplier ^double randomization-factor]
    (IntervalFunction/ofExponentialRandomBackoff initial-interval-millis multiplier randomization-factor)))
 
 ;; -----------------------------------------------------------------------------
 ;; registry
 
-(def registry
+(def default-registry
   "The global retry and config registry."
   (RetryRegistry/ofDefaults))
 
 (defn- build-configs-map [configs-map]
   (into {} (map (fn [[k v]] [(clojure.core/name k) (build-config v)]) configs-map)))
 
-(defn configure-registry!
-  "Overwrites the global registry with one that contains the configs-map.
+(defn registry
+  "Creates a registry with default values or a map of nam/config-map pairs."
+  ([]
+   (RetryRegistry/ofDefaults))
+  ([configs-map]
+   (let [^Map configs (build-configs-map configs-map)]
+     (RetryRegistry/of configs))))
 
-  configs-map is a map whose keys are names and vals are configs. When a retry
-  is created, you may specify one of the names in this map to use as the config
-  for that retry.
+(defn all-retries
+  "Gets all the retries in `registry`.
 
-  :default is a special name. It will be used as the config for retrys that do
-  not specify a config to use."
-  [configs-map]
-  (alter-var-root (var registry)
-                  (fn [_]
-                    (RetryRegistry/of (build-configs-map configs-map)))))
+  Uses [[default-registry]] if `registry` is not provided."
+  ([]
+   (all-retries default-registry))
+  ([^RetryRegistry registry]
+   (set (.getAllRetries registry))))
+
+(defn add-configuration!
+  "Adds `config` to the `registry` under the `name`.
+
+  Uses [[default-registry]] if `registry` is not provided."
+  ([name config]
+   (add-configuration! default-registry name config))
+  ([^RetryRegistry registry name config]
+   (.addConfiguration registry (clojure.core/name name) (build-config config))))
+
+(defn find
+  "Finds the retry identified by `name` in `registry`.
+
+  Uses [[default-registry]] if `registry` is not provided."
+  ([name]
+   (find default-registry name))
+  ([^RetryRegistry registry name]
+   (optional-value (.find registry (clojure.core/name name)))))
+
+(defn remove!
+  "Removes the retry identified by `name` from `registry`.
+
+  Uses [[default-registry]] if `registry` is not provided."
+  ([name]
+   (remove! default-registry name))
+  ([^RetryRegistry registry name]
+   (optional-value (.remove registry (clojure.core/name name)))))
+
+(defn replace!
+  "Replaces the retry identified by `name` in `registry` with the specified `retry`.
+
+  Uses [[default-registry]] if `registry` is not provided."
+  ([name ^Retry retry]
+   (replace! default-registry name retry))
+  ([^RetryRegistry registry name ^Retry retry]
+   (optional-value (.replace registry (clojure.core/name name) retry))))
+
+;; -----------------------------------------------------------------------------
+;; registry events
+
+(defn- entry-added-consumer [out-chan]
+  (reify EventConsumer
+    (consumeEvent [_ event]
+      (let [^EntryAddedEvent e event]
+        (async/offer! out-chan
+                      {:event-type (keywordize-enum-value (.getEventType e))
+                       :added-entry ^Retry (.getAddedEntry e)})))))
+
+(defn- entry-removed-consumer [out-chan]
+  (reify EventConsumer
+    (consumeEvent [_ event]
+      (let [^EntryRemovedEvent e event]
+        (async/offer! out-chan
+                      {:event-type (keywordize-enum-value (.getEventType e))
+                       :removed-entry ^Retry (.getRemovedEntry e)})))))
+
+(defn- entry-replaced-consumer [out-chan]
+  (reify EventConsumer
+    (consumeEvent [_ event]
+      (let [^EntryReplacedEvent e event]
+        (async/offer! out-chan
+                      {:event-type (keywordize-enum-value (.getEventType e))
+                       :old-entry ^Retry (.getOldEntry e)
+                       :new-entry ^Retry (.getNewEntry e)})))))
+
+(def registry-event-types
+  "The event types that can be raised by a registry."
+  #{:added
+    :removed
+    :replaced})
+
+(defn emit-registry-events!
+  "Offers registry events to `out-chan`.
+
+  The event types are identified by [[registry-event-types]].
+
+  This function also accepts `:only` and `:exclude` keyword params that are
+  sequences of the event types that should be included or excluded,
+  respectively.
+
+  Uses [[default-registry]] if `registry` is not provided."
+  ([out-chan]
+   (emit-registry-events! default-registry out-chan))
+  ([^RetryRegistry registry out-chan & {:keys [only exclude]
+                                        :or {exclude []}}]
+   (let [events-to-publish (if only (set only)
+                               (apply disj registry-event-types exclude))
+         ^Registry$EventPublisher pub (.getEventPublisher registry)]
+     (when (contains? events-to-publish :added)
+       (.onEntryAdded pub (entry-added-consumer out-chan)))
+     (when (contains? events-to-publish :removed)
+       (.onEntryRemoved pub (entry-removed-consumer out-chan)))
+     (when (contains? events-to-publish :replaced)
+       (.onEntryReplaced pub (entry-replaced-consumer out-chan))))
+   out-chan))
 
 ;; -----------------------------------------------------------------------------
 ;; creation and lookup
 
 (defn retry!
   "Creates or fetches a retry with the specified name and config and stores it
-  in the global registry.
+  in `registry`.
 
   The config value can be either a config map or the name of a config map stored
-  in the global registry.
+  in the registry. If the retry already exists in the registry, the config value
+  is ignored. 
 
-  If the retry already exists in the global registry, the config value is
-  ignored."
+  Uses [[default-registry]] if `registry` is not provided."
   ([name]
-   {:pre [(s/valid? ::name name)]}
+   (retry! default-registry name))
+  ([^RetryRegistry registry name]
    (.retry registry (clojure.core/name name)))
-  ([name config]
-   {:pre [(s/valid? ::name name)
-          (s/valid? (s/or :name ::name :config ::config) config)]}
-   (if (s/valid? ::name config)
+  ([^RetryRegistry registry name config]
+   (if (name? config)
      (.retry registry (clojure.core/name name) (clojure.core/name config))
-     (.retry registry (clojure.core/name name) (build-config config)))))
+     (let [^RetryConfig cfg (build-config config)]
+       (.retry registry (clojure.core/name name) cfg)))))
 
 (defn retry
-  "Creates a retry with the specified name and config."
-  [name config]
-  (Retry/of (clojure.core/name name) (build-config config)))
+  "Creates a retry with the `name` and `config`."
+  ([name]
+   (retry name {}))
+  ([name config]
+   (let [^RetryConfig cfg (build-config config)]
+     (Retry/of (clojure.core/name name) cfg))))
 
 ;; -----------------------------------------------------------------------------
 ;; execution
@@ -174,7 +289,71 @@
   provide a name and a retry of that name does not already exist in the global
   registry, one will be created with the `:default` config."
   [retry & body]
-  `(let [r# (if (s/valid? ::name ~retry)
-              (retry! (clojure.core/name ~retry))
-              ~retry)]
+  `(let [r# (if (instance? Retry ~retry)
+              ~retry
+              (retry! (clojure.core/name ~retry)))]
      (execute r# (fn [] ~@body))))
+
+;; -----------------------------------------------------------------------------
+;; retry properties
+
+(defn name
+  "Gets the name of `retry`."
+  ([^Retry retry]
+   (.getName retry)))
+
+;; -----------------------------------------------------------------------------
+;; retry events
+
+(def event-types
+  #{:retry
+    :success
+    :error
+    :ignored-error})
+
+(defn- base-event [^AbstractRetryEvent event]
+  {:event-type (keywordize-enum-value (.getEventType event))
+   :retry-name (.getName event)
+   :creation-time (.getCreationTime event)
+   :number-of-retry-attempts (.getNumberOfRetryAttempts event)
+   :last-throwable (.getLastThrowable event)})
+
+(defn- retry-consumer [out-chan]
+  (reify EventConsumer
+    (consumeEvent [_ event]
+      (let [^RetryOnRetryEvent e event]
+        (async/offer! out-chan
+                      (assoc (base-event e)
+                             :wait-interval (.getWaitInterval e)))))))
+
+;; Note: success, error, and ignored-error do not add any additional fields, so
+;; we just use a base consumer for them.
+(defn- base-consumer [out-chan]
+  (reify EventConsumer
+    (consumeEvent [_ event]
+      (let [^AbstractRetryEvent e event]
+        (async/offer! out-chan
+                      (base-event e))))))
+
+(defn emit-events!
+  "Offers events on `retry` to `out-chan`.
+
+  The event types are identified by [[event-types]].
+
+  This function also accepts `:only` and `:exclude` keyword params that are
+  sequences of the event types that should be included or excluded,
+  respectively."
+  [^Retry retry out-chan & {:keys [only exclude]
+                            :or {exclude []}}]
+  (let [events-to-publish (if only
+                            (set only)
+                            (apply disj event-types exclude))
+        ^Retry$EventPublisher pub (.getEventPublisher retry)]
+    (when (contains? events-to-publish :retry)
+      (.onRetry pub (retry-consumer out-chan)))
+    (when (contains? events-to-publish :success)
+      (.onSuccess pub (base-consumer out-chan)))
+    (when (contains? events-to-publish :error)
+      (.onError pub (base-consumer out-chan)))
+    (when (contains? events-to-publish :ignored-error)
+      (.onIgnoredError pub (base-consumer out-chan)))))
